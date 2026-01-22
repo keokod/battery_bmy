e3a3674bc0a13a9ded566ba9ab32954e386d880b
keokod <keo.n@free.fr>
2026-01-22 13:10:49 +0100

    Update out_gp16_PIN21_pico2.py

--- out_gp16_PIN21_pico2.py
+++ out_gp16_PIN21_pico2.py
@@ -1,5 +1,12 @@
 from machine import Pin
 import time
+led = Pin(16, mode=Pin.OUT) # la broche numéro 21 est la gpio 16
+while True:
+ led.toggle()
+ time.sleep(1)
+ print("clign")
+from machine import Pin
+import time
 led = Pin(16, mode=Pin.OUT)
 while True:
  led.toggle()
